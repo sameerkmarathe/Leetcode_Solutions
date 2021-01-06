c1dd0b4db088c5d2d02bd4bb57626093e18e78b9
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-01-06 06:41:58 -0500

    java solution for problem 1
    Only included the most efficient approach for the java file

---
+++ 1/main.java
@@ -0,0 +1,22 @@
+/*
+
+		1. Two Sum 
+
+*/
+
+/*
+Please refer to the python file for more information on the approach
+*/
+
+Map<Integer,Integer> indDict = new HashMap<>();
+for(int i = 0; i < nums.length; i++) {
+    if(indDict.containsKey(target-nums[i])) {
+        return new int[]{i, indDict.get(target-nums[i])};
+
+    } else {
+        indDict.put(nums[i], i);
+    }
+
+}
+return null;
+
