ba96e036a9466389483ce5cd914cdffc6ce97823
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-01-13 10:47:28 -0500

    First attempt for problem 11
    Added the information about the approach for this problem

---
+++ 11/main.py
@@ -0,0 +1,12 @@
+#######################################################################
+################	11. Container With Most Water	###############
+#######################################################################
+
+'''
+Classic two pointer problem. We can brute force to check every combination but this is slow. It is much 
+better to encroach the space where we store the water represented as two pointers. 
+Note the maximum capacity along the process.
+Time Complexity: O(n) because we visit each element once.
+Space Complexity: O(1) because we only need constant amount of space to store the information. 
+'''
+
