c26a811adfd2bc8db199ec89feefa833f6b321b4
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-03-04 17:07:55 -0500

    Solution for problem 27

---
+++ 27/main.java
@@ -0,0 +1,23 @@
+/***
+*
+*	27. Remove Element
+*
+***/
+
+
+/*
+Refer to python solution for approach
+*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+    }
+}
