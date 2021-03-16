78166298bab01e0c1595dcbed02cd1732dddd4ac
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-03-16 17:10:30 -0400

    Adding the solution python for 31

---
+++ 31/main.py
@@ -0,0 +1,30 @@
+##################################################
+#############	31. Next Permutation	##########
+##################################################
+
+
+
+
+
+'''
+Just cater for one reverse part when we find the ascending numbers in the list. Reverse if no ascending position was found
+Time Complexity:O(n) in case we need to reverse numbers
+Space Complexity:O(1) We use constant space to keep track of ascending position
+'''
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return 
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        l, r = k+1, len(nums)-1  # reverse the second part
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
