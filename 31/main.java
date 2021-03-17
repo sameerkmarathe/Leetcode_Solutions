bbfd1138cecd54e80904c576d3fa129d3e46264d
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-03-17 17:13:57 -0400

    Java for 31

---
+++ 31/main.java
@@ -0,0 +1,42 @@
+/**
+*
+*	31. Next Permutation
+*
+**/
+
+
+/*
+Check the python code for more information on complexity
+*/
+
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+}
