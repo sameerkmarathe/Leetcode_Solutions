90e7a498dfdb65c0f2bce966195ca7b87da54032
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-03-19 17:29:04 -0400

    solution for 32

---
+++ 32/main.java
@@ -0,0 +1,32 @@
+/***
+*
+*	32. Longest Valid Parantheses
+*
+**/
+
+/*
+Refer to the python solution for complexity
+*/
+
+
+public class Solution {
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
