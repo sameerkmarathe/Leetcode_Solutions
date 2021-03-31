81463fe99d46aee259e435b7aa7d013ed9575207
sameerkmarathe <marathe.k.sameer@gmail.com>
2021-03-31 17:14:55 -0400

    python for 36

---
+++ 36/main.py
@@ -0,0 +1,41 @@
+#####################################################
+###########	36. Valid Sudoku	#############
+#####################################################
+
+
+
+'''
+Just check the validity in every row, column and square
+Time Complexity: O(n^2) n is the length of board
+Space Complexity: O(1) as we do not store state
+'''
+
+class Solution:
+    def isValidSudoku(self, board):
+        return (self.is_row_valid(board) and
+                self.is_col_valid(board) and
+                self.is_square_valid(board))
+
+    def is_row_valid(self, board):
+        for row in board:
+            if not self.is_unit_valid(row):
+                return False
+        return True
+
+    def is_col_valid(self, board):
+        for col in zip(*board):
+            if not self.is_unit_valid(col):
+                return False
+        return True
+
+    def is_square_valid(self, board):
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_unit_valid(square):
+                    return False
+        return True
+
+    def is_unit_valid(self, unit):
+        unit = [i for i in unit if i != '.']
+        return len(set(unit)) == len(unit)
